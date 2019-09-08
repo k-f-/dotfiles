554773d69a5de6b203eb7983fab4be20841da01c
Kyle Fring <me@kfring.com>
2019-09-08 17:56:38 -0400

    duh

--- scripts/disp-external-and-laptop.sh
+++ scripts/disp-external-and-laptop.sh
@@ -4,4 +4,4 @@ xrandr --dpi 276 \
        --fb 7680x2160 \
        --output eDP1 --primary --mode 3840x2160 --rate 60 \
        --output HDMI1 --mode 1920x1080 --rotate normal --scale-from 3840x2160 --panning 3840x2160+3840+0 --right-of eDP1
-./.bin/set-wallpaper
+sh ~/.bin/set-wallpaper
