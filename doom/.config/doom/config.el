477a7fe3218e998826da7082bc73fe5afffd17b7
Kyle Fring <me@kfring.com>
2020-07-20 02:08:23 -0400

    .

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -98,3 +98,15 @@
 	smtpmail-smtp-service 587
 	smtpmail-debug-info t)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(send-mail-function (quote mailclient-send-it)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
