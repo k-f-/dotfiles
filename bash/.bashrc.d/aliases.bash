8e5a4f2e203952f76251c7e7b2f938bf217573f3
Kyle Fring <kyle@fring.io>
2022-05-17 00:33:30 -0400

    fix broken alias in zsh, i never use it anyway

--- bash/.bashrc.d/aliases.bash
+++ bash/.bashrc.d/aliases.bash
@@ -43,7 +43,7 @@ alias agr="sudo apt remove"
 alias acs="apt search"
 alias agu="sudo apt update && sudo apt full-upgrade && sudo apt autoremove && sudo apt autoclean"
 alias ali="apt-mark showmanual"
-alias pkgcount="sudo dpkg-query -f '${binary:Package}\n' -W | wc -l"
+#alias pkgcount="sudo dpkg-query -f '${binary:Package}\n' -W | wc -l"
 
 alias oports="echo 'User:      Command:   Port:'; echo '----------------------------' ; lsof -i 4 -P -n | grep -i 'listen' | awk '{print \$3, \$1, \$9}' | sed 's/ [a-z0-9\.\*]*:/ /' | sort -k 3 -n |xargs printf '%-10s %-10s %-10s\n' | uniq"
 alias serve="python -m SimpleHTTPServer"
