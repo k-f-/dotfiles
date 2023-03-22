7d9f0799283348afa29ac07dd2d4becd1cadc70f
Kyle Fring <kyle@fring.io>
2023-03-22 16:52:52 -0400

    weird timestamp in install-mac.sh

--- scripts/install-mac.sh
+++ scripts/install-mac.sh
@@ -1,4 +1,4 @@
 # useful url: https://www.alchemists.io/projects/mac_os-config/
 
-defaults write com.apple.screencapture name kef-screenshot                                                                                     =14:34:07
+defaults write com.apple.screencapture name kef-screenshot        
 defaults write com.apple.screencapture location /Users/kef/Documents/kef/Screenshots
