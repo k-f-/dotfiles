8bd8da172482c601d6308545541f82f5e10d1b61
Kyle Fring <kyle@fring.io>
2023-10-08 16:53:19 -0400

    mbp2+1

--- scripts/install-mac.sh
+++ scripts/install-mac.sh
@@ -1,4 +1,5 @@
 # useful url: https://www.alchemists.io/projects/mac_os-config/
 
 defaults write com.apple.screencapture name kef-screenshot        
-defaults write com.apple.screencapture location /Users/kef/Documents/kef/Screenshots
+defaults write com.apple.screencapture location ~/Documents/Screenshots
+xattr -d com.apple.quarantine /Applications/SpaceId.app
